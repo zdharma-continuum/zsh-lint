222741f73ba76de77fbb6a5ecb62f64824177cc8
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-09-25 09:12:52 +0200

    zsh-lint.plugin.zsh

---
+++ zsh-lint.plugin.zsh
@@ -0,0 +1,12 @@
+# Copyright (c) 2019 Sebastian Gniazdowski
+# License MIT
+
+# Handle $0 according to the Zsh Plugin Standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+typeset -g ZSHLINT_REPO_DIR="${0:h}"
+typeset -g ZSHLINT_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/zsh-lint"
+
+autoload zsh-lint
